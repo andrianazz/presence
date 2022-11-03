d1603a02299dcc35ff71145131367986788cd307
andrianazz <andrianwahyu41@gmail.com>
2022-11-04 00:50:17 +0700

    add login

---
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:presence/app/routes/app_pages.dart';
+
+class LoginController extends GetxController {
+  TextEditingController emailC = TextEditingController();
+  TextEditingController passC = TextEditingController();
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  void login() async {
+    if (emailC.text.isNotEmpty && passC.text.isNotEmpty) {
+      try {
+        final credential = await auth.signInWithEmailAndPassword(
+          email: emailC.text,
+          password: passC.text,
+        );
+
+        print(credential);
+
+        if (credential.user != null) {
+          if (credential.user!.emailVerified == true) {
+            Get.offAllNamed(Routes.HOME);
+          } else {
+            Get.defaultDialog(
+              title: "Belum Verifikasi",
+              middleText: 'Kamu belum verifikasi akun ini',
+            );
+          }
+        }
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          Get.snackbar("Terjadi Kesalahan", "Email belum terdaftar");
+        } else if (e.code == 'wrong-password') {
+          Get.snackbar("Terjadi Kesalahan", "Password Salah");
+        }
+      } catch (e) {
+        Get.snackbar("Terjadi Kesalahan", "Tidak dapat login");
+      }
+    } else {
+      Get.snackbar("Terjadi Kesalahan", "Email dan Password wajib diisi");
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: "Application",
-      initialRoute: AppPages.INITIAL,
+      initialRoute: Routes.LOGIN,
       getPages: AppPages.routes,
     );
   }
